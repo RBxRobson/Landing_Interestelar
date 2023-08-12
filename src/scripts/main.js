c64d13e338758f83d335d2eea55113cfa731ae9a
Robson.B <robsonnbarbosa0@gmail.com>
2023-08-12 18:59:46 -0300

    finalizando section críticas

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menu = document.querySelectorAll('[data-menu-open]');
+    const containerCarousel = document.getElementById('carousel');
+    const image = document.querySelectorAll('#carousel img ');
+    let x = 0;
+    const criticasListItem = document.querySelectorAll('.criticas__list__item');
+
     for (let i = 0; i < menu.length; i++) {
-        menu[i].addEventListener('click', openOrClose);
+        menu[i].addEventListener('click', menuOpenOrClose);
     }
+
+    function carousel() {
+        x++;
+        if (x > image.length - 1) {
+            x = 0;
+        }
+        containerCarousel.style.transform = `translateX(${-x * 100}%)`;
+    }
+    setInterval(carousel, 2000);
+
+    criticasListItem.forEach((item) => {
+        const autor = item.querySelector('.autor');
+
+        autor.addEventListener('click', () => {
+            const critica = item.querySelector('.critica');
+            const criticaOpen = document.querySelector('.critica--is-open');
+
+            verify(item, critica, criticaOpen);
+        });
+    });
+
 })
 
-function openOrClose() {
-    const classe = 'content__menu--is-open';
-    const nav = document.querySelector('.content__menu')
+function menuOpenOrClose() {
+    const classe = 'header__menu--is-open';
+    const nav = document.querySelector('.header__menu');
     nav.classList.toggle(classe);
 }
 
-const containerCarousel = document.getElementById('carousel');
-const image = document.querySelectorAll('#carousel img ');
+function verify(item, critica, criticaOpen) {
+    const iconItem = item.querySelector('.icon');
+    const icons = document.querySelectorAll('.icon');
+    icons.forEach((item) => (item.innerHTML = "+"));
 
-let x = 0;
-
-function carousel() {
-    x++;
-    if (x > image.length - 1) {
-        x = 0;
+    if (criticaOpen) {
+        criticaOpen.style.height = 0;
+        criticaOpen.classList.remove('critica--is-open');
     }
 
-    containerCarousel.style.transform = `translateX(${-x * 100}%)`;
+    if (critica !== criticaOpen) {
+        iconItem.innerHTML = "-";
+        critica.classList.add('critica--is-open');
+        critica.style.height = critica.scrollHeight + 10 + "px";
+    }
 }
-
-setInterval(carousel, 2000);
